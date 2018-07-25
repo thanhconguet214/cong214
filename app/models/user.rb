d2403611133e8d47cfc619854aca4bfa45c80504
Cong <thanhcong.uet97@gmail.com>
2018-07-25 10:47:56 +0700

    Make a basic User model (including secure passwords)

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,2 @@
 class User < ApplicationRecord
-	validates :name, presence: true
-	has_secure_password
 end
---
+++ db/migrate/20180725034540_add_password_digest__users.rb
@@ -0,0 +1,4 @@
+class AddPasswordDigestUsers < ActiveRecord::Migration[5.2]
+  def change
+  end
+end
